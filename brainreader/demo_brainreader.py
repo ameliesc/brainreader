582f906eb2573e04a5508fb7cb73fe5ea67ec557
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-03-08 15:59:53 +0100

    getting started

---
+++ brainreader/demo_brainreader.py
@@ -0,0 +1,41 @@
+from plotting.data_conversion import put_data_in_grid
+from theano.gof.graph import Variable
+from brainreader.art_gallery import get_image
+from brainreader.pretrained_networks import get_vgg_net
+import numpy as np
+from matplotlib import pyplot as plt
+
+__author__ = 'peter'
+
+
+def im2feat(im): return im.dimshuffle('x', 2, 0, 1) if isinstance(im, Variable) else np.rollaxis(im, 2, 0)[None, :, :, :]
+
+def feat2im(feat): return feat.dimshuffle(0, 2, 3, 1)[0, :, :, :] if isinstance(feat, Variable) else np.rollaxis(feat, 0,2)[0, :, :, :]
+
+def normalize(arr): return (arr - np.mean(arr)) / np.std(arr)
+
+
+def demo_brainreader():
+
+    # Need to get dataset.  See from scipy.io import loadmat
+
+    raw_content_image = get_image('starry_night', size=(128, None))
+    input_im = normalize(im2feat(raw_content_image))
+    net = get_vgg_net(up_to_layer='conv2_1')
+    func = net.compile()
+    feat = func(input_im)  # shape (n_samples, n_maps, size_y, size_x)
+
+    plt.subplot(2,1,1)
+    plt.imshow(raw_content_image)
+    plt.title('Image')
+    plt.subplot(2,1,2)
+    plt.imshow(put_data_in_grid(feat.reshape(16, 8, *feat.shape[2:])), cmap='gray', interpolation = 'nearest')
+    plt.title('Features')
+    plt.show()
+
+    # You can use sklearn for Linear Regression
+
+
+
+if __name__ == '__main__':
+    demo_brainreader()
