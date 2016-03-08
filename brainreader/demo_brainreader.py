d478110e08b6ddf78c201f7e205fc034ba9eda3f
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-03-08 16:28:33 +0100

    added some comments

--- brainreader/demo_brainreader.py
+++ brainreader/demo_brainreader.py
@@ -17,24 +17,28 @@ def normalize(arr): return (arr - np.mean(arr)) / np.std(arr)
 
 def demo_brainreader():
 
-    # Need to get dataset.  See from scipy.io import loadmat
+    # Need to get dataset.  See from scipy.io import loadmat.  Example in pretrained_networks.py may be useful.
 
-    raw_content_image = get_image('starry_night', size=(128, None))
-    input_im = normalize(im2feat(raw_content_image))
-    net = get_vgg_net(up_to_layer='conv2_1')
-    func = net.compile()
-    feat = func(input_im)  # shape (n_samples, n_maps, size_y, size_x)
+    raw_content_image = get_image('starry_night', size=(128, None))  # (im_size_y, im_size_x, n_colours)
+    input_im = normalize(im2feat(raw_content_image))  # (n_samples, n_colours, im_size_y, im_size_x)  -- where n_samples=1 and n_colours=3
+    net = get_vgg_net(up_to_layer='conv2_1')  # See function get_vgg_net for the layers that you can go up to.
+    func = net.compile()  # Compile the network into a function that takes input_im and returns features.
+    feat = func(input_im)  # shape (n_samples, n_feat_maps, feat_size_y, feat_size_x) -- where n_samples=1
 
-    plt.subplot(2,1,1)
+    # Plot
+    plt.subplot(2, 1, 1)
     plt.imshow(raw_content_image)
     plt.title('Image')
-    plt.subplot(2,1,2)
-    plt.imshow(put_data_in_grid(feat.reshape(16, 8, *feat.shape[2:])), cmap='gray', interpolation = 'nearest')
+    plt.subplot(2, 1, 2)
+    plt.imshow(put_data_in_grid(feat[0]), cmap='gray', interpolation = 'nearest')
     plt.title('Features')
     plt.show()
 
-    # You can use sklearn for Linear Regression
+    # You can use sklearn for Linear Regression.
 
+    # For the deconv stuff, I have a similar project that does gradient descent on the image (it's not exactly deconvolution
+    # but similar) example code in:
+    # https://github.com/petered/liquid-style/blob/move-on-over/liquid_style/demo_liquid_style.py
 
 
 if __name__ == '__main__':
