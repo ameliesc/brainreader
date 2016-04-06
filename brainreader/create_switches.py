27894dda4aeaaeed723c0245cdb15e7864bc3420
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-04-06 13:17:46 +0200

    create switches function

---
+++ brainreader/create_switches.py
@@ -0,0 +1,37 @@
+import numpy as np
+from general.ezprofile import EZProfiler
+
+
+def creat_switches(relu_out, pool_out, stride, region):
+    pool = pool_out[0][0]
+    relu = relu_out[0][0]
+    rowp, columnp = pool.shape[-2:]
+    rowr, columnr = relu.shape[-2:]
+    k = 0
+    region_end_k = range(region[0])[-1]
+    region_end_l = range(region[1])[-1]
+    switch_matrix = np.zeros(relu.shape)
+    i = 0
+    while i < rowp:
+        j = 0
+        region_beg_k = k
+        region_end_k = region_beg_k + range(region[0])[-1]
+        while j < columnp:
+            max_val = pool[i][j]
+            l = 0
+            while k < region_end_k:
+                region_beg_l = l
+                region_end_l = region_beg_l + range(region[1])[-1]
+                print "reastart l-loop"
+                while l < region_end_l:
+                    ## need to account for the case that same number occurs twice
+                    if relu[k][l] ==  max_val:
+                        switch_matrix[k][l] = 1
+                        print switch_matrix[k][l]
+                    l += 1
+                l = region_beg_l + stride[1]
+                k += 1
+            k = region_beg_k + stride[0]
+            j +=1
+        i +=1
+    return switch_matrix, k, i
