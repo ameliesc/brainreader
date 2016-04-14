492345b3bceed08a7485ec73251cf613c5244642
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-04-14 11:00:17 +0200

    put everything together

--- brainreader/conv_reg_deconv.py
+++ brainreader/conv_reg_deconv.py
@@ -7,7 +7,14 @@ from brainreader.art_gallery import get_image
 import numpy as np
 from matplotlib import pyplot as plt
 import theano
+import h5py
+from scipy.misc import imresize
+from data_preprocessing import get_data
 from collections import OrderedDict
+from plotting.data_conversion import put_data_in_grid
+from sklearn import linear_model
+from scipy.io import loadmat
+from matplotlib import pyplot as plt
 
 def im2feat(im):
     """
@@ -15,6 +22,8 @@ def im2feat(im):
     :returns: A (1, 3, size_y, size_x) array representing the BGR image that's ready to feed into VGGNet
 
     """
+    im = imresize(im, (224, 224))  #update to vggnet requirements ;incorporate normalize 
+    im = (np.dstack((im, im, im)))
     centered_bgr_im = im[:, :, ::-1] - np.array([103.939, 116.779, 123.68])
     feature_map_im = np.rollaxis(centered_bgr_im, 2, 0)[None, :, :, :]
     return feature_map_im.astype(theano.config.floatX)
@@ -29,25 +38,58 @@ def feat2im(feat):
     decentered_rgb_im = (bgr_im + np.array([103.939, 116.779, 123.68]))[:, :, ::-1]
     return decentered_rgb_im
 
-def demo_brainreader():
-    deconv = load_conv_and_deconv()
-    raw_content_image = get_image('starry_night', size=(224, 224))  # (im_size_y, im_size_x, n_colours)
+def demo_brainreader(sample_size = 1750, layer_name = 'pool1'):
+    ### get data ###
+    
+    stimuli_train, response_train = get_data(response=1)
+    input_im = np.empty([1, 3, 224, 224])
+    input_im[0] = im2feat(stimuli_train[0])
 
-    input_im = im2feat(raw_content_image)
-    net = get_vgg_net(up_to_layer='pool1')
-
+    ### Convolution ###
+    
+    net = get_vgg_net(up_to_layer = layer_name)
     func = net.get_named_layer_activations.compile()
-
     named_features = func(input_im)
+    feat = np.squeeze(named_features[layer_name + '_layer'])
+    
+    ### Regression ###
+    
+    regr_x = np.empty((sample_size, feat.shape[0] *feat.shape[1] * feat.shape[2])) # (1750, n_maps, size_y, size_x)
+    regr_x[0] = np.reshape(feat,(feat.shape[0] *feat.shape[1] * feat.shape[2]))
+    for i in range(1, sample_size):
+        input_im[0] =im2feat(stimuli_train[i])
+        named_features = func(input_im)
+        feat = np.squeeze(named_features[layer_name + '_layer'])
+        print feat.shape
+        print (sample_size, feat.shape[0] *feat.shape[1] * feat.shape[2])
+        #pickle.dump(feat,open("featuremaps.p", 'w'))
+        regr_x[i] = np.reshape(feat,(feat.shape[0] *feat.shape[1] * feat.shape[2]))
+
+    regr = linear_model.LinearRegression()
+    # X needs n samples and n features
+    regr_model = regr.fit(regr_x, response_train[:sample_size])
+
+    ### Deconvolution 
     switch_dict = OrderedDict()
     for name in named_features:
         
         if 'switch' in name:
             switch_dict[name] = named_features[name]
-    net = get_deconv(switch_dict, network_params=deconv, from_layer= 'pool1')
 
+    deconv = load_conv_and_deconv()
+    net = unwrap_deconvnet(switch_dict, network_params=deconv, from_layer= layer_name)
     func = net.compile()
+    image_reconstruct = func( np.reshape(regr_model.coef_,feat) * feat)
 
-    image_reconstruct = func(named_features['pool1_layer'])
+    ### Plotting ###
+    plt.subplot(2, 1, 1)
+    plt.imshow(raw_content_image)
+    plt.title('Image')
+    plt.subplot(2, 1, 2)
+     # plt.imshow(put_data_in_grid(named_features[layer][0]),
+     #cmap='gray', interpolation = 'nearest')
+    plt.imshow(feat2im(image_reconstruct, cmap = 'gray'))
+    plt.title('Features')
+    plt.show()
 
     return image_reconstruct
