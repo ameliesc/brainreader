dce1f2b6964214f8a00f666dba93613b10b01a7f
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-04-20 15:43:33 +0200

    added train and test

--- brainreader/regression_features.py
+++ brainreader/regression_features.py
@@ -1,21 +1,87 @@
-import numpy as np
-from scipy.io import loadmat
-from matplotlib import pyplot as plt
 from brainreader.pretrained_networks import get_vgg_net
-from plotting.data_conversion import put_data_in_grid
-from sklearn import linear_model
+from brainreader.unwrap_deconvnet import get_deconv
+from brainreader.makedeconvnet import load_conv_and_deconv
+from theano.gof.graph import Variable
+import numpy as np
+import theano
 import h5py
+import cPickle as pickle
 from scipy.misc import imresize
 from data_preprocessing import get_data
+from collections import OrderedDict
+from scipy.io import loadmat, savemat
 
-__author__ = 'amelie'
+
+def im2feat(im):
+    """
+    :param im: A (size_y, size_x, 3) array representing a RGB image on a [0, 255] scale
+    :returns: A (1, 3, size_y, size_x) array representing the BGR
+    image that's ready to feed into VGGNet
+
+    """
+    im = imresize(
+        im, (224, 224))  # update to vggnet requirements ;incorporate normalize
+    im = (np.dstack((im, im, im)))
+    centered_bgr_im = im[:, :, ::-1] - np.array([103.939, 116.779, 123.68])
+    feature_map_im = np.rollaxis(centered_bgr_im, 2, 0)[None, :, :, :]
+    return feature_map_im.astype(theano.config.floatX)
 
 
-def setup_net():
-    net = get_vgg_net(up_to_layer='fc8')  # See function get_vgg_net for the layers that you can 
-    func = net.compile()
-    return func
+def get_featuremaps(sample_size=1750, layer_name=None, data_set='train'):
 
-def get_feat(imput_im,func):
-    feat = func(imput_im)
-    return feat
+    ### get data ###
+    if data_set == 'train':
+        stimuli_train = get_data()
+    else:
+        stimuli_train = get_data(data='test')
+    input_im = np.empty([1, 3, 224, 224])
+    input_im[0] = im2feat(stimuli_train[0])
+
+
+    ####  Case get all layers 
+    if layer_name == None:
+
+        layer_names = ['conv1_1', 'conv1_2', 'conv2_1', 'conv2_2',
+                       'conv3_1', 'conv3_2',  'conv3_3',  'conv3_4', 'conv4_1',
+                       'conv4_2', 'conv4_3',  'conv4_4', 'conv5_1',
+                       'conv5_2',  'conv5_3',  'conv5_4',  'fc6',  'fc7',
+                       'fc8']
+
+        
+        
+        feature_maps = OrderedDict()
+        for l_name in layer_names:
+            input_im[0] = im2feat(stimuli_train[0])
+            net = get_vgg_net(up_to_layer = l_name )
+            func = net.get_named_layer_activations.compile()
+            print 'Convolving image up to %s' % (l_name)
+            named_features = func(input_im)
+            feat = named_features[l_name + '_layer']
+            regr_x  = np.empty((sample_size, feat.shape[1] * feat.shape[2] * feat.shape[3]))
+            regr_x[0]  = np.reshape(feat, (feat.shape[1] * feat.shape[2] * feat.shape[3]))
+            for i in range(1, sample_size):
+                input_im[0] = im2feat(stimuli_train[i])
+                named_features = func(input_im)
+                regr_x[0]  = np.reshape(feat, (feat.shape[1] * feat.shape[2] * feat.shape[3]))
+            feature_maps[l_name] = regr_x
+    
+        print "Saving Feature maps to matlab file..."
+        savemat('feature_maps_%s.mat' % (data_set), {'feature_maps': feature_maps})
+        print "Done"
+    #### Case just get one layer
+    else:
+        feat = np.squeeze(named_features[layer_name + '_layer'])
+        # (1750, n_maps, size_y, size_x)
+        regr_x = np.empty(
+            (sample_size, feat.shape[0] * feat.shape[1] * feat.shape[2]))
+        regr_x[0] = np.reshape(
+            feat, (feat.shape[0] * feat.shape[1] * feat.shape[2]))
+        for i in range(1, sample_size):
+            input_im[0] = im2feat(stimuli_train[i])
+            named_features = func(input_im)
+            feat = np.squeeze(named_features[layer_name + '_layer'])
+            regr_x[i] = np.reshape(
+                feat, (feat.shape[0] * feat.shape[1] * feat.shape[2]))
+        feature_maps = regr_x
+    print 'Finished.'
+    return feature_maps
