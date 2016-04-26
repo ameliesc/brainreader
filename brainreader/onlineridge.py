a87baff6281c4412741e93fd6432744625a56df7
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-04-26 14:51:05 +0200

    included stepsize and momentum

--- brainreader/onlineridge.py
+++ brainreader/onlineridge.py
@@ -9,10 +9,11 @@ def online_ridge():
 
     #['conv1_1', 'conv1_2', 'conv2_1', 'conv2_2',
     #'conv3_1', 'conv3_2',  'conv3_3',  'conv3_4', 'conv4_1','conv4_2', 'conv4_3',  'conv4_4', 'conv5_1',
-                 #  'conv5_2',  'conv5_3',  'conv5_4',
-    layer_names = [  'fc6',  'fc7',
-                   'fc8']
+                 #  'conv5_2',  'conv5_3',   'conv5_4'
+    layer_names = ['fc6', 'fc7','fc8']
 
+    regr_coef  = OrderedDict()
+    regr_cost = OrderedDict()
     for name in layer_names:
 
         print "load featuremap for training.."
@@ -25,34 +26,101 @@ def online_ridge():
         x_train = feature_map_train
         y_test = get_data(response=1, data='test')  # n_samples x n_targets
         x_test = feature_map_test  # n_samples x n_feature
-
+        batch_size = 100
         n_in = x_train.shape[1]
         n_out = y_train.shape[1]
         n_training_samples = x_train.shape[0]
         n_test_samples = x_test.shape[0]
         i = 0
-        score_report_period = 200
-        n_epochs = 1
-#0 2.7580e-25, 2.7198e-13, 6.5068e-08, 7.2153e+18,6.8359e+22,[   1.5407e+47, 4.5480e+51, 9.8656e+53, 7.9408e+58]
-        lmbda_list = [0.001]
-        for l in lmbda_list:
+        score_report_period = 400
+        n_epochs = 2
+        lmbda = 0.01
 
-            predictor = LinearRegressor(n_in, n_out, lmbda = l, eta = 0.00000000000000000000000000000000000000000000000001)
-            f_train = predictor.train.compile()
-            f_predict = predictor.predict.compile()
-            f_cost = predictor.cost.compile()
-            # Train on one sample at a time and periodically report score.
-            for i in xrange(n_training_samples*n_epochs+1):#
-                #if i % score_report_period == 0:
-                out = f_predict(x_test)
-                w = predictor.coef_()
-                w = w.get_value()
-                test_cost = ((y_test - out)**2).sum(axis = 1).mean(axis=0) 
+        predictor = LinearRegressor(n_in, n_out, lmbda = lmbda, eta = 0.01 )
+        f_train = predictor.train.compile()
+        f_predict = predictor.predict.compile()
+        set_params = predictor.set_params.compile()
+        f_cost = predictor.voxel_cost.compile()
+        f_cost(x_test,y_test)
+        # Train on one sample at a time and periodically report score.
+        epoch = 0
+        test_cost_old = 0
+        roh = 1.1
+        alpha = 0.3
+        w_old = None
+        
+        while i < n_training_samples*n_epochs+1:
+            out = f_predict(x_test)
+            test_cost = ((y_test - out)**2).sum(axis = 1).mean(axis=0) 
+            #skip NaN reslts
+            if test_cost_old < test_cost:
+                test_cost = test_cost
+            elif test_cost_old > test_cost or test_cost_old == test_cost:
+                test_cost = test_cost
+            else:
+                print "Cost nan or inf resetting parameters."
+                
+                # predictor = LinearRegressor(n_in, n_out, lmbda = lmbda, eta = alpha*eta, w=  w_old )
+                # f_train = predictor.train.compile()
+                # f_predict = predictor.predict.compile()
+                predictor.set_params(alpha=alpha)
+                i = 0
+                epoch = 0
+                eta = predictor.get_params()
+                eta = eta.get_value()
+                print "new step size: %s" % (eta)
+            if i % score_report_period == 0:
                 print 'Test-Cost at epoch %s: %s' % (float(i)/n_training_samples, test_cost)
-                if np.isnan(test_cost):
-                    return w
-                f_train(x_train[[i % n_training_samples]], y_train[[i % n_training_samples]])
-            return w
+            eta = predictor.get_params()
+            if i == epoch: # Adaptive Stepsize
+ 
+                if test_cost_old < test_cost:
+                   
+                    #predictor = LinearRegressor(n_in, n_out, lmbda = lmbda, eta = eta.eval(), w=  w_old ) # decrease learning rate by alpha
+                    #f_train = predictor.train.compile()
+                    #f_predict = predictor.predict.compile()
+                    #
+                    print "Cost too high resetting parameters."
+                    eta = predictor.get_params()
+                    eta = eta.get_value()
+                    predictor.set_params(alpha=alpha, eta = eta, w = w_old)
+                    print "new step size: %s" % (eta)
+                    #set_params(alpha = alpha)
+
+                elif test_cost_old > test_cost or test_cost_old == test_cost:
+                    eta = predictor.get_params()
+                    eta = eta.get_value()
+                    predictor.set_params(roh = roh, eta = eta)
+                    print "Current learning rate: %s" % (eta)
+                        
+                else:
+                    print "Cost nan or inf resetting parameters."
+                    eta = predictor.get_params()
+                    eta = eta.get_value()
+                    predictor.set_params(alpha=alpha, eta = eta, w = w_old)
+                    print "new step size: %s" % (eta)
+                    # predictor = LinearRegressor(n_in, n_out, lmbda = lmbda, eta = alpha*eta, w=  w_old )# decrease learning rate by alpha
+                    # f_train = predictor.train.compile()
+                    # f_predict = predictor.predict.compile()
+                    i = 0
+                    epoch = 0
+
+                test_cost_old = test_cost
+                w = predictor.coef_()
+                w_old = w.get_value()
+                epoch += n_training_samples
+            f_train(x_train[[i % n_training_samples]], y_train[[i % n_training_samples]])
+            i += 1
+
+                
+        w = predictor.coef_()
+        w_old = w.get_value()
+        cost_voxel = f_cost(x_test,y_test)
+        regr_cost["name"] = cost_voxel
+        regr_coef["name"] =  w_old
+            
+    dd.io.save("regression_coefficients_fc.h5", regr_coef)
+    dd.io.save("regression_cost_fc.h5", regr_cost)
             #print f_cost(x_test,y_test) 
             #return f_cost(x_test, y_test) # return values for testing cost function in commandline
                 #print "Cost train: %d" % (f_cost(x_train, y_train[:,i:i+10]))
