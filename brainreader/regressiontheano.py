543cc36fa402ea54b3ac47264260c5a701e7ca4e
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-04-26 14:56:11 +0200

    ds

--- brainreader/regressiontheano.py
+++ brainreader/regressiontheano.py
@@ -4,26 +4,46 @@ import numpy as np
 
 class LinearRegressor:
 
-    def __init__(self, n_in, n_out, lmbda, eta = 0.01):
-        self.w = create_shared_variable(np.zeros((n_in, n_out)))
-        self.eta = eta
+    def __init__(self, n_in, n_out, lmbda, eta= 0.01, w = None):
+        if w is not None:
+            self.w = create_shared_variable(w)
+        else:
+            self.w = create_shared_variable(np.zeros((n_in, n_out)))
         self.lmbda = lmbda
+        self.cost = None
+        self.eta = create_shared_variable(eta)
 
     @symbolic
-    def train(self, x, targ):  # x: (n_samples, n_in), targ: (n_samples, n_out)
+    def set_params(self,roh=1, alpha =1, eta = None, w = None):
+        if eta is not None:
+            eta_new = roh * eta *alpha
+            self.eta.set_value(eta_new)
+        if w is not None:
+            self.w.set_value(w)
+        
+    @symbolic
+    def train(self, x, targ):# x: (n_samples, n_in), targ: (n_samples, n_out)       
         y = self.predict(x)
-        cost  =  (((targ - y)**2) + (self.lmbda * (self.w  ** 2).sum(axis = 0))).sum(axis=1).mean(axis=0)
-        add_update(self.w, self.w - self.eta*tt.grad(cost=cost, wrt=self.w))
+        if self.cost == None:
+            delta_w_old = 0
+            beta = 0
+        else:
+            beta = 0.95
+            delta_w_old = self.w - self.eta*tt.grad(cost=self.cost, wrt=self.w) # old gradient
+        self.cost  =  (((targ - y)**2) + (self.lmbda * (self.w  ** 2).sum(axis = 0))).sum(axis=1).mean(axis=0)
+        add_update(self.w, beta * delta_w_old + (1-beta)*(- self.eta*tt.grad(cost=self.cost, wrt=self.w))) # update with momentum
 
     @symbolic
     def predict(self, x):  # x: (n_samples, n_in)
         return x.dot(self.w)
 
     @symbolic
-    def cost(self, x,y_true):
-        y = self.predict(x)
-        cost = (((y_true - y)**2).sum(axis=0)
-        return  cost
+    def voxel_cost(self, x,y_true):
+        y = self.predict(x) 
+        return ((y_true - y)**2).sum(axis=0)
+
+    def get_params(self):
+        return self.eta
 
     def coef_(self):
         return self.w
