407137206ec9776e7747037c69f93112e7fefcb1
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-05-02 16:59:08 +0200

    multiproc

--- brainreader/online_ridgev2.py
+++ brainreader/online_ridgev2.py
@@ -3,20 +3,17 @@ from data_preprocessing import get_data
 from collections import OrderedDict
 import deepdish as dd
 from regressionridgev2 import LinearRegressor
+import multiprocessing
 
 
 
 
 
-
-def online_ridge(region, mini_batch_size = 10, batch_size = 10, method = "Adam", stepsize = 0.000001, name = 'fc7', lmbda = 0.01):
-
-    # 
-    layer_names = [name]
+def online_ridge(region, mini_batch_size = 100, batch_size = 10, method = "Adam", stepsize = 0.000001, name = 'fc6', lmbda = 0.01):
 
     regr_coef  = OrderedDict()
     regr_cost = OrderedDict()
-    for name in layer_names:
+
 
         print "load featuremap for training.."
         feature_map_batch1= dd.io.load("featuremaps_train_0_%s.h5" % (name))
@@ -44,7 +41,7 @@ def online_ridge(region, mini_batch_size = 10, batch_size = 10, method = "Adam",
         n_training_samples = 1 #x_train.shape[0]
         n_test_samples = x_test.shape[0]
         score_report_period = 350
-        n_epochs = 10
+        n_epochs = 7
         lmbda = lmbda
         cost_voxel = np.zeros_like(y_test)
         weights_voxel = np.zeros((x_train.shape[1],y_train.shape[1]))
@@ -64,54 +61,29 @@ def online_ridge(region, mini_batch_size = 10, batch_size = 10, method = "Adam",
             
            
             while i < n_training_samples*n_epochs+1:
-                try:
-                    if i % score_report_period == 0:
-                        out = f_predict(x_test)
-                        test_cost = ((y_test[:,j : j+ batch_size] - out)**2).sum(axis = 1).mean(axis=0)
-                        print 'Test-Cost at epoch %s: %s' % (float(i)/n_training_samples, test_cost)
 
-                    f_train(x_train[i: i+sample_batch_size,:], y_train[i % n_training_samples, j: j+batch_size])
-                    i += sample_batch_size
+                #if i % score_report_period == 0:
+                 #   out = f_predict(x_test)
+                  #  test_cost = ((y_test[:,j : j+ batch_size] - out)**2).sum(axis = 1).mean(axis=0)
+                   # print 'Test-Cost at epoch %s: %s' % (float(i)/n_training_samples, test_cost)
 
-                       
-                except KeyboardInterrupt:
-                    print 'All done'
-                    break
-
-                #if i == epoch:
-                    #if  abs(test_cost_old - test_cost) < 1e-9 and epoch > 6 *  n_training_samples: #stopping condition
-                        # print "im loop"
-                        # print test_cost
-                        # print cost_min
-                        # if test_cost != cost_min:
-                            
-                        #     print "stuck in local minimum, resetting weights randomly"
-                        #     predictor = LinearRegressor(n_in, n_out, lmbda = lmbda, w =0 )
-                        #     f_train = predictor.train.compile()
-                        #     f_predict = predictor.predict.compile()
-                        #     f_cost = predictor.voxel_cost.compile()
-                        #     epoch = 0
-                        #     i = 0
-                        #     test_cost = 0
-                        #     test_cost_old = 0
-                    
-                    #    else:
-                        #print "No learning, move to next batch"
-                        #break
-                    #test_cost_old = test_cost    
-                    #epoch += n_training_samples
-
-                
-
+                f_train(x_train[i: i+sample_batch_size,:], y_train[i % n_training_samples, j: j+batch_size])
+                i += sample_batch_size
+         
             cost_batch = f_cost(x_test, y_test[:,j:j+batch_size])
             cost_voxel[:,j:j+batch_size] =cost_batch
             w = predictor.parameters
             weights_voxel[:,j:j+batch_size] = w[0].get_value()
             j = j + batch_size
-        regr_cost[name] = cost_voxel
-        regr_coef[name] = weights_voxel
         regr_predictions = f_predict(x_test)
+        dd.io.save("regression_coefficients_roi%s_%s.h5" % (roi,name), regr_predictions)
         dd.io.save("regression_coefficients_roi%s_%s.h5" % (roi,name), weights_voxel)
         dd.io.save("regression_cost_roi%s_%s.h5" % (roi,name), cost_voxel)
 
+
+
+pool = multiprocessing.Pool(processes=2)
+region = [1,2,3,4,5,6,7]
+pool.map(online_ridge,region)
+pool.close()
 
