1c46b9ec6a42671e1b5081df04c878d25c513699
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-05-05 15:52:51 +0200

    reset batch loading after epoch

--- brainreader/onlineridge_lowerlayers.py
+++ brainreader/onlineridge_lowerlayers.py
@@ -62,7 +62,7 @@ def online_ridge(region=1, mini_batch_size = 100, batch_size = 10, method = "Ada
                 feature_map_train= dd.io.load("/data/featuremaps_train_%s_%s.h5" % (n,name))
                 x_train = np.nan_to_num((feature_map_train-np.mean(feature_map_train, axis=1)[:, None])/np.std(feature_map_train, axis=1)[:, None])
                 n += 1
-                if i == 1750: #reset batch loading, one epoch passed
+                if i % 1750 == 0: #reset batch loading, one epoch passed
                     n = 0
 
            
