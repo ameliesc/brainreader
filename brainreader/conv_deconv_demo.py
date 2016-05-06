e6a3a8c58b03d3616cdecd62a43df4875e64450c
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-05-06 15:22:13 +0200

    maybe solved bug, my laptop cant handle

--- brainreader/conv_deconv_demo.py
+++ brainreader/conv_deconv_demo.py
@@ -1,9 +1,8 @@
-from brainreader.pretrained_networks import get_vgg_net
-from brainreader.unwrap_deconvnet import get_deconv
-from brainreader.makedeconvnet import load_conv_and_deconv
+from pretrained_networks import get_vgg_net
+from unwrap_deconvnet import get_deconv
+from makedeconvnet import load_conv_and_deconv
 from plotting.data_conversion import put_data_in_grid
 from theano.gof.graph import Variable
-from brainreader.art_gallery import get_image
 import numpy as np
 from matplotlib import pyplot as plt
 import theano
