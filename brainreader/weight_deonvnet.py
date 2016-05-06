72f86aeb37480f727d07736036908c9c7b07c418
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-05-06 17:42:40 +0200

    yaaas fixed it ggogogo

--- brainreader/weight_deonvnet.py
+++ brainreader/weight_deonvnet.py
@@ -58,7 +58,7 @@ def feat2im(feat):
     decentered_rgb_im = (bgr_im + np.array([103.939, 116.779, 123.68]))[:, :, ::-1]
     return decentered_rgb_im
 
-def conv_and_deconv(layername, n, , voxel_index):
+def conv_and_deconv(layername, n, voxel_index):
 
     #raw_content_image = get_image('trump', size=(224, 224))  # (im_size_y, im_size_x, n_colours)
     #input_im = im2feat(raw_content_image)
