f9375ee95919649343936165c6b822f7442c5a62
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-05-06 22:09:15 +0200

    veryfy that deconv works

--- brainreader/weight_deonvnet.py
+++ brainreader/weight_deonvnet.py
@@ -20,15 +20,6 @@ import deepdish as dd
 from matplotlib.backends.backend_pdf import PdfPages
 from analyse import filtervoxels
 
-def im2feat(im):
-    """
-    :param im: A (size_y, size_x, 3) array representing a RGB image on a [0, 255] scale
-    :returns: A (1, 3, size_y, size_x) array representing the BGR image that's ready to feed into VGGNet
-
-    """
-    centered_bgr_im = im[:, :, ::-1] - np.array([103.939, 116.779, 123.68])
-    feature_map_im = np.rollaxis(centered_bgr_im, 2, 0)[None, :, :, :]
-    return feature_map_im.astype(theano.config.floatX)
 
 def im2feat(im):
     """
@@ -133,41 +124,59 @@ def layer_images():
             net = get_deconv(switch_dict, network_params=deconv_net, from_layer= layername)
             deconv = net.compile()
             image_reconstructed = deconv(features)
-            raw_content_image = feat2im(im2feat(stimuli_test[0]))
-            plt.figure(figsize = (8,3))
+            #test only showing strongest activation#
+            maxval = np.amax(image_reconstruct, axis = 1)
+            zeroed = np.asarray(image_reconstruct)
+            indices = zeroed < maxval
+            zeroed[indices] = 0
+
             plt.subplot(2, 1, 1)
-            plt.imshow(raw_content_image, cmap='Greys_r')
-            plt.title('Original Image')
+            plt.imshow(raw_content_image)
+            plt.title('Image')
             plt.subplot(2, 1, 2)
-            plt.imshow(feat2im(image_reconstructed), cmap='Greys_r')
-            plt.title('Reconstuction of voxel %s' % (index[0]))
-            pp.savefig()
-            for j in index[1:]:
-                for k in range(1,120): 
-                    stimuli_test = get_data(data='test')
-                    input_im = np.empty([1, 3, 224, 224])
-                    input_im = im2feat(stimuli_test[k])
-                    named_features = conv(input_im)
+            # plt.imshow(put_data_in_grid(named_features[layer][0]),
+            #cmap='gray', interpolation = 'nearest')
+            plt.imshow(feat2im(zeroed))
+            plt.title('Features')
+            plt.show()
+            plt.imshow(feat2im(image))
+            plt.title('Features')
+            plt.show()
+            # raw_content_image = feat2im(im2feat(stimuli_test[0]))
+            # plt.figure(figsize = (8,3))
+            # plt.subplot(2, 1, 1)
+            # plt.imshow(raw_content_image, cmap='Greys_r')
+            # plt.title('Original Image')
+            # plt.subplot(2, 1, 2)
+            # plt.imshow(feat2im(image_reconstructed), cmap='Greys_r')
+            # plt.title('Reconstuction of voxel %s' % (index[0]))
+            # pp.savefig()
+            # for j in index[1:]:
+            #     for k in range(1,120): 
+            #         stimuli_test = get_data(data='test')
+            #         input_im = np.empty([1, 3, 224, 224])
+            #         input_im = im2feat(stimuli_test[k])
+            #         named_features = conv(input_im)
 
-                    switch_dict = OrderedDict()
-                    for name in named_features:
+            #         switch_dict = OrderedDict()
+            #         for name in named_features:
 
-                        if 'switch' in name:
-                            switch_dict[name] = named_features[name]
+            #             if 'switch' in name:
+            #                 switch_dict[name] = named_features[name]
 
-                    features =  named_features[layername+'_layer']
+            #         features =  named_features[layername+'_layer']
 
-                    weights = dd.io.load('/data/regression_coefficients_roi%s_%s.h5' % (i, layername))
-                    w_times_feat = features * np.reshape(weights[:, j],features.shape)
-                    features = w_times_feat
-                    image_reconstructed = deconv(features)
-                    raw_content_image = feat2im(im2feat(stimuli_test[k]))
-                    plt.figure()
-                    plt.subplot(2, 1, 1)
-                    plt.imshow(raw_content_image, cmap='Greys_r')
-                    plt.title('Original Image')
-                    plt.subplot(2, 1, 2)
-                    plt.imshow(feat2im(image_reconstructed), cmap='Greys_r')
-                    plt.title('Reconstuction of voxel %s' % (j))
-                    pp.savefig()
-            pp.close()
+            #         weights = dd.io.load('/data/regression_coefficients_roi%s_%s.h5' % (i, layername))
+            #         w_times_feat = features * np.reshape(weights[:, j],features.shape)
+            #         features = w_times_feat
+            #         image_reconstructed = deconv(features)
+            #         raw_content_image = feat2im(im2feat(stimuli_test[k]))
+            #         plt.figure()
+            #         plt.subplot(2, 1, 1)
+            #         plt.imshow(raw_content_image, cmap='Greys_r')
+            #         plt.title('Original Image')
+            #         plt.subplot(2, 1, 2)
+            #         plt.imshow(feat2im(image_reconstructed), cmap='Greys_r')
+            #         plt.title('Reconstuction of voxel %s' % (j))
+            #         pp.savefig()
+            # pp.close()
