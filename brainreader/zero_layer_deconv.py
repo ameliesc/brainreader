765c595494298f2d46576d442a983dabf66f8d53
Amelie Schmidt-Colberg <amelie.schmidt-colberg@student.auc.nl>
2016-05-07 12:32:33 +0200

    testing

--- brainreader/zero_layer_deconv.py
+++ brainreader/zero_layer_deconv.py
@@ -97,8 +97,10 @@ def demo_brainreader(layername):
         deconv_net = load_conv_and_deconv()
         net = get_deconv(switch_dict, network_params=deconv_net, from_layer= layername)
         max_act = np.amax(named_features[layername+'_layer'], axis = 1)
+        return named_features
         zeroed = np.asarray(named_features[layername+'_layer'])
         indices = zeroed < max_act
+        return zeroed, indices
         zeroed[indices] = 0
         return zeroed
         deconv = net.compile()
